2f9cec6539364b57d3bc3973d518be219b1282d3
uduma sonia <sohnyauduma@gmail.com>
2025-06-17 23:30:28 +0100

    habit search

--- src/lib/components/Dashboard/DashboardView.svelte
+++ src/lib/components/Dashboard/DashboardView.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 	import NavBar from '../Common/NavBar.svelte';
+	import HabitSearch from '../Habit/HabitSearch.svelte';
 </script>
 
 <div class="mx-auto w-full max-w-[1000px]">
 	<NavBar />
+	<div class="mt-4">
+		<HabitSearch />
+	</div>
 </div>
---
+++ src/lib/components/Habit/HabitSearch.svelte
@@ -0,0 +1,15 @@
+<div class="flex gap-3">
+	<div class="max-w-[300px] flex-1">
+		<input
+			placeholder="Search tracker"
+			type="text"
+			class="h-[50px] w-full max-w-[300px] rounded-lg border-2 border-black px-3 outline-none placeholder:font-light"
+		/>
+	</div>
+
+	<div>
+		<a href="/dashboard" class="block">
+			<button class="shadow_button h-[46px]"> Search </button>
+		</a>
+	</div>
+</div>
