0c712f002a2750524ca88a75ffb08c66c4902b7a
uduma sonia <sohnyauduma@gmail.com>
2025-06-23 16:54:54 +0100

    habit tracker: Filter form

--- src/lib/components/Habit/Utilities/FilterForm.svelte
+++ src/lib/components/Habit/Utilities/FilterForm.svelte
@@ -1,1 +1,86 @@
-<div class="h-[50px] border-2 border-black">FILYER</div>
+<script lang="ts">
+	let showStatusDropdown = $state(false);
+
+	const openStatus = () => {
+		showStatusDropdown = true;
+	};
+
+	const closeStatus = () => {
+		showStatusDropdown = false;
+	};
+</script>
+
+<div class="flex max-w-[200px] flex-1 gap-3">
+	<div class="max-w-[200px] flex-1">
+		<div class="dropdown_wrapper" class:dropdown_open={showStatusDropdown}>
+			<input
+				placeholder="Pending"
+				type="text"
+				value="Pending"
+				onclick={openStatus}
+				onblur={closeStatus}
+				class="relative z-10 h-[50px] w-full max-w-[350px] cursor-pointer rounded-lg border-2 border-black bg-white px-3 outline-none placeholder:font-light"
+			/>
+
+			<img src="/images/arrow_down_thick.svg" class="absolute top-2.5 right-2 z-50 w-8" alt="" />
+
+			{#if showStatusDropdown}
+				<div class="py2 relative z-50 px-1">
+					<button
+						class="font-lexend hover:bg-brand-lime h-[40px] w-full rounded-md px-3 text-left font-light"
+					>
+						All
+					</button>
+					<button
+						class="font-lexend hover:bg-brand-lime h-[40px] w-full rounded-md px-3 text-left font-light"
+					>
+						Pending
+					</button>
+					<button
+						class="font-lexend hover:bg-brand-lime h-[40px] w-full rounded-md px-3 text-left font-light"
+					>
+						Completed
+					</button>
+					<button
+						class="font-lexend hover:bg-brand-lime h-[40px] w-full rounded-md px-3 text-left font-light"
+					>
+						Stopped
+					</button>
+				</div>
+			{/if}
+		</div>
+	</div>
+</div>
+
+<style>
+	.dropdown_wrapper {
+		position: relative;
+		border-radius: 8px;
+		transition: all 0.4s linear;
+	}
+
+	.dropdown_open {
+		background-color: white;
+		transition: all 0.4s linear;
+		left: 0px;
+	}
+
+	.dropdown_open::before {
+		background-color: white;
+		left: 0px !important;
+	}
+
+	.dropdown_wrapper::before {
+		content: '';
+		position: absolute;
+		top: 5px;
+		left: 5px;
+		right: 0;
+		bottom: 0;
+		border: 2px solid black;
+		z-index: 1;
+		border-radius: 8px;
+		width: 100%;
+		height: 100%;
+	}
+</style>
--- src/lib/components/Habit/View.svelte
+++ src/lib/components/Habit/View.svelte
@@ -10,7 +10,7 @@
 	<NavBar />
 	<NameCard />
 	<DateScroller />
-	<div class="mt-3 flex items-center gap-6">
+	<div class="mt-3 flex items-start gap-6">
 		<HabitSearch />
 		<FilterForm />
 	</div>
