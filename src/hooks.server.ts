5b85653f5385aa5d1b0580607826964e25f7f52d
uduma sonia <sohnyauduma@gmail.com>
2025-07-18 07:11:53 +0100

    login route guard

---
+++ src/hooks.server.ts
@@ -0,0 +1,22 @@
+import { AUTH_TOKEN } from '$lib/constants/global';
+import type { Handle } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
+
+export const handle: Handle = async ({ event, resolve }) => {
+	const token = event.cookies.get(AUTH_TOKEN);
+
+	if (event.route.id?.includes('private')) {
+		if (!token) {
+			throw redirect(302, '/login');
+		}
+	}
+
+	if (event.url.pathname === '/login' || event.url.pathname === '/') {
+		if (token) {
+			throw redirect(302, '/tracker');
+		}
+	}
+
+	const response = await resolve(event);
+	return response;
+};
--- src/lib/components/Auth/LoginForm.svelte
+++ src/lib/components/Auth/LoginForm.svelte
@@ -1,6 +1,42 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { AUTH_TOKEN } from '$lib/constants/global';
+	import { AuthRequest } from '$lib/requests';
+	import { addToast } from '$lib/store/toast';
+	import Helpers from '$lib/utils/helpers';
+
+	let formElement = $state();
+	let email = $state('');
+	let username = $state('');
+	let password = $state('');
+	let isSubmitting = $state(false);
+
+	async function handleSubmit(e: any) {
+		e.preventDefault();
+		try {
+			isSubmitting = true;
+
+			const result = await AuthRequest.register({ username, password, email, avatar: '' });
+
+			if (result) {
+				Helpers.setCookie(AUTH_TOKEN, result?.data?.token, 400000);
+				goto('/tracker');
+			}
+		} catch (error: any) {
+			addToast(error?.error || 'An error occured', 'error');
+		} finally {
+			isSubmitting = false;
+		}
+	}
+</script>
+
 <div class="flex h-full items-center justify-center px-4">
 	<div class="login_form_wrapper w-full md:max-w-[400px]">
-		<div class="login_form h-full rounded-3xl border-2 bg-white p-4">
+		<form
+			onsubmit={handleSubmit}
+			bind:this={formElement}
+			class="login_form h-full rounded-3xl border-2 bg-white p-4"
+		>
 			<div class="pb-3">
 				<p class="font-suez text-3xl">Login</p>
 			</div>
@@ -11,7 +47,11 @@
 				<div>
 					<label for="email" class="mb-2">Email Address</label>
 					<input
+						bind:value={email}
 						type="email"
+						id="email"
+						name="email"
+						autocomplete="email"
 						class="h-[50px] w-full rounded-lg border-2 border-black px-3 outline-none"
 					/>
 				</div>
@@ -19,6 +59,10 @@
 					<label for="email" class="mb-2">Password</label>
 					<input
 						type="password"
+						bind:value={password}
+						id="password"
+						name="password"
+						autocomplete="new-password"
 						class="h-[50px] w-full rounded-lg border-2 border-black px-3 outline-none"
 					/>
 				</div>
@@ -33,6 +77,6 @@
 					Don't have an account? <a href="/signup" class="font-medium underline">Sign up</a>
 				</p>
 			</div>
-		</div>
+		</form>
 	</div>
 </div>
