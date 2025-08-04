58d2b3b9dd91a628d4cbd6d31d6a60da385a97e1
uduma sonia <sohnyauduma@gmail.com>
2025-08-04 06:23:27 +0100

    habit tracker: tracker details/stats modal

--- src/lib/components/Habit/DetailsModal.svelte
+++ src/lib/components/Habit/DetailsModal.svelte
@@ -4,11 +4,18 @@
 
 	let { onClose, isOpen } = $props();
 
+	let details = $derived(trackerState.data.trackerDetails);
+
 	$effect(() => console.log($state.snapshot(trackerState.data.trackerDetails)));
 </script>
 
-<ModalWrapper {onClose} {isOpen}>
-	<div>
-		<p class="font-lexend text-xl">Standard list</p>
+<ModalWrapper {onClose} {isOpen} maxWidth="max-w-[1000px]">
+	<div class="flex items-center gap-2">
+		<div>
+			{#if details?.icon}
+				<img src={details.icon} alt="walk icon" class="h-5" />
+			{/if}
+		</div>
+		<p class="font-lexend text-lg sm:text-xl">{details?.name}</p>
 	</div>
 </ModalWrapper>
