b8970f03782089ff7a3c2b50353a9f64e89f9d5b
uduma sonia <sohnyauduma@gmail.com>
2025-10-06 18:30:35 +0100

    empty state component

---
+++ src/lib/components/Common/EmptyState.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { Hourglass } from '@lucide/svelte';
+
+	let { heading, text, link, buttonText } = $props();
+</script>
+
+<div class="flex h-[300px] items-center justify-center">
+	<div>
+		<div class="mx-auto mt-4 mb-2 w-fit">
+			<Hourglass />
+		</div>
+
+		<p class="font-lexend text-13 text-center font-light">
+			<span class="font-semibold">
+				{heading}
+			</span>
+
+			<br />
+
+			{text}
+		</p>
+
+		<div class="mx-auto mt-6 w-[200px]">
+			<a href={link}>
+				<button class="shadow_button shadow_button_sm" style="height: 40px" type="button">
+					{buttonText}
+				</button>
+			</a>
+		</div>
+	</div>
+</div>
