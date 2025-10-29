b173e17014c018c3bf832c13f7492b85d7dd9bb9
uduma sonia <sohnyauduma@gmail.com>
2025-10-29 10:58:20 +0100

    fix modal title component

--- src/lib/components/Habit/Utilities/ModalTitle.svelte
+++ src/lib/components/Habit/Utilities/ModalTitle.svelte
@@ -3,9 +3,11 @@
 </script>
 
 <div class="flex items-center gap-1.5">
-	<div>
-		<img src={iconUrl} class="h-6 min-w-8" alt={name} />
-	</div>
+	{#if iconUrl}
+		<div>
+			<img src={iconUrl} class="h-6 min-w-8" alt={name} />
+		</div>
+	{/if}
 
 	<p class="font-lexend text-lg sm:text-xl">
 		{name}
