084cc8fa29752559dbe4a476d59a714801b0727b
uduma sonia <sohnyauduma@gmail.com>
2025-11-21 11:39:21 +0100

    fix meal planner responsiveness

--- src/lib/components/MealPlanner/Utilities/WeekLabels.svelte
+++ src/lib/components/MealPlanner/Utilities/WeekLabels.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	let {} = $props();
 </script>
 
 {#snippet dayButton(title: string)}
@@ -7,23 +6,6 @@
 		{title}
 	</div>
 {/snippet}
-
-{#snippet timeOfDay(title: string)}
-	<div
-		class="flex h-full items-center justify-center border-r border-l bg-[#cfc4e7] px-4 text-center text-sm md:text-base"
-	>
-		{title}
-	</div>
-{/snippet}
-
-<div class="absolute top-0 left-0 z-30 h-[40px] w-full">
-	<div class="font-lexend grid h-full grid-cols-4 pl-[50px]">
-		{@render timeOfDay('Breakfast')}
-		{@render timeOfDay('Lunch')}
-		{@render timeOfDay('Dinner')}
-		{@render timeOfDay('Snacks')}
-	</div>
-</div>
 
 <div class="absolute top-0 left-0 z-30 h-full w-[50px] pt-[40px]">
 	<div>
